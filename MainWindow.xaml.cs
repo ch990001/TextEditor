444e9f0788114af5d66e47a1a115bc0099b0dd98
ch990001 <ch990001@gmail.com>
2023-05-29 13:48:53 +0800

    進行介面基本設計
    控制項：下拉選單控制項

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,6 +23,12 @@ namespace TextEditor
         public MainWindow()
         {
             InitializeComponent();
+            // 清除rtbText的內容
+            rtbText.Document.Blocks.Clear();
+            // 設定字型下拉選單的選單內容，存取你的電腦裡面的字型庫，將你安裝的字型清單都放進去
+            cmbFontFamily.ItemsSource = Fonts.SystemFontFamilies.OrderBy(f => f.Source);
+            // 設定字體大小下拉選單的選單內容，設定8`72的數字，這要用來設定字體大小
+            cmbFontSize.ItemsSource = new List<double>() { 8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 36, 48, 72 };
         }
     }
 }
